8d8c788d6d545fc3032430a29a88b67fb5dd7979
Gavrilo Mumovic <gajo357@gmail.com>
2023-03-02 20:59:24 +0100

    Canvas

--- index.js
+++ index.js
@@ -56,3 +56,73 @@ function pisi() {
   element.style.display = "inline-block";
   // element.style.visibility = "visible";
 }
+
+function dodaj_krug() {
+  // nadji canvas element
+  const canvas = document.getElementById("mojKanvas");
+  // uzmi kontekst za crtanje 2D
+  const ctx = canvas.getContext("2d");
+
+  // nasumicna pozicija od 0 do sirine canvas-a 200px
+  const x = Math.round(Math.random() * canvas.width);
+  // nasumicna pozicija od 0 do visine canvas-a 100px
+  const y = Math.round(Math.random() * canvas.height);
+  // nasumicni poluprecnik kruga do 50px
+  const radius = Math.round(Math.random() * 50);
+  nacrtaj_krug(ctx, x, y, radius);
+}
+
+function nacrtaj_krug(ctx, x, y, radius) {
+  // zapocni crtanje
+  ctx.beginPath();
+  // opisi krug
+  // x, y, radius, startAngle, endAngle
+  ctx.arc(x, y, radius, 0, 2 * Math.PI);
+  // iscrtaj jednom linijom
+  ctx.stroke();
+}
+
+let interval = 0;
+function pokreni_simulaciju() {
+  // nadji canvas element
+  const canvas = document.getElementById("mojKanvas");
+  // uzmi kontekst za crtanje 2D
+  const ctx = canvas.getContext("2d");
+
+  // fps - frames per second
+  // broj osvjezavanja u sekundi
+  // sve preko 24 oko nece primjetiti da ponovo iscrtavamo
+  // probajte sa 10 ili sa 30
+  const fps = 15;
+  // brzina kretanja kruga (pixel/second)
+  // moramo podijeliti sa fps da bismo dobili brzinu po osvjezavanju
+  const brzina = 50 / fps;
+
+  // pocetne pozicije x, y
+  let x = 0; // koristimo "let" jer cemo da mijenjamo ovu vrijednost
+  let y = 0;
+  const radius = 10; // koristimo "const" jer radius necemo da mijenjamo
+
+  // obrisi bilo koju postojecu simulaciju
+  zaustavi_simulaciju();
+
+  function iscrtaj_na_novoj_poziciji() {
+    x += brzina;
+    y += brzina;
+    // obrisi canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    nacrtaj_krug(ctx, x, y, radius);
+  }
+  // nacrtaj krug na pocetnoj poziciji
+  nacrtaj_krug(ctx, x, y, radius);
+  // interval stavimo u varijablu "interval" da bismo ga mogli obrisati
+  // setInterval prima milisekunde, a "fps" je u sekundama, zato dijelimo 1000.0 sa fps
+  interval = setInterval(iscrtaj_na_novoj_poziciji, 1000.0 / fps);
+}
+
+function zaustavi_simulaciju() {
+  // "interval" je broj, ako je veci od 0 znaci da postoji i da ga mozemo izbrisati
+  if (interval > 0) {
+    clearInterval(interval);
+  }
+}
